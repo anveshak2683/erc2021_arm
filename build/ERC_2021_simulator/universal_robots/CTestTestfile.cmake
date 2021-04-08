de978e6f8e588047bb0bd30cc2aea307a0a693a1
Akshat Joshi <akshatjoshi375@gmail.com>
2021-04-08 23:20:01 +0530

    Removed build and devel

--- build/ERC_2021_simulator/universal_robots/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /projects/arm/src/ERC_2021_simulator/universal_robots
-# Build directory: /projects/arm/build/ERC_2021_simulator/universal_robots
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
